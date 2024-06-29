fc9c3e43d0288921f2252caa462a77937f50b623
Maximilian Bronkhorst <maximilian.bronkhorst@protonmail.com>
2024-06-29 14:06:27 +0200

    added get method

--- LinkedList.py
+++ LinkedList.py
@@ -42,11 +42,48 @@ class LinkedList:
                 self.tail = None
                 return temp.value
 
+    def prepend_Item(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+              self.head = new_node
+              self.tail = new_node
+        else:
+              new_node.next = self.head
+              self.head = new_node
+              self.length += 1
+              return True
 
-main_LL = LinkedList(1)
+    def pop_first_Item(self):
+        if self.length == 0:
+                return None
+        temp = self.head
+        self.head = self.head.next 
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+              self.tail = None
+        return temp
+    
+    def get_Item(self, index):
+        if index < 0 or index >= self.length:
+              return None  
+        temp = self.head
+        for _ in range(index):
+              temp = temp.next
+        return temp.value
 
-main_LL.append_Item(7)
 
+
+main_LL = LinkedList(0)
+
+main_LL.append_Item(1)
+main_LL.append_Item(2)
+
+main_LL.get_Item(1)
 #main_LL.pop_Item()
 
-main_LL.print_list()
+#main_LL.prepend_Item(8)
+
+#main_LL.pop_first_Item()
+
+#main_LL.print_list()
