3d81b221a3859add0302aaa3c02a927603c2ea80
max <maximilian.bronkhorst@protonmail.com>
2025-01-28 12:51:35 +0100

    added stacks and queues

--- DoubleLinkedLIst.py
+++ DoubleLinkedLIst.py
@@ -80,8 +80,62 @@ class DoubleLinkedList:
                 temp = temp.prev  
         return temp
     
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        else:
+            return False
+        
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+ 
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+ 
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        
+        self.length += 1   
+        return True          
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+ 
+        temp = self.get(index)
+        
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+ 
+        self.length -= 1
+        return temp
+
+    def swap_first_last(self):
+        if self.head is None:
+            return None
+        temp = self.head.value
+        self.head.value = self.tail.value
+        self.tail.value = temp            
+
 main_DLL = DoubleLinkedList(1)
 main_DLL.append(2)
 main_DLL.append(3)
+main_DLL.append(4)
 main_DLL.print_list()
 print('\n')
